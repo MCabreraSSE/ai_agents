9e3b416c539048d15c40b503a419fe5ef5f05b5a
Martin <4lch4p4@gmail.com>
2025-02-26 17:58:32 +0000

    Resuelto conflicto

--- hf_smolagents/01 - first project.py
+++ hf_smolagents/01 - first project.py
@@ -2,6 +2,12 @@ from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
 from dotenv import load_dotenv
 load_dotenv()
 
+
+model = HfApiModel(
+model_id='Qwen/Qwen2.5-Coder-32B-Instruct',
+provider=None,
+)
+
 agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
 
 agent.run("How long would it take for an elephant to cross the united states from florida to california?")
